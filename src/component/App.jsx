3c0a067e533dfe394e41cfe72a0e554e7f06420f
juhua15 <1024853976@qq.com>
2019-10-16 01:25:02 +0800

    修改重定向

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,24 +1,45 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Route, Link ,Redirect} from "react-router-dom"
+import { Row, Col } from 'antd';
 import HeadCarouse from "./headCarousel"
 
 import ImageList from "./imageList"
 
 function Index() {
-  return <h2>Home</h2>;
+  return (
+    <div>
+      <h2>我们的产品</h2>
+      <HeadCarouse></HeadCarouse>
+      <ImageList></ImageList>
+    </div>
+   
+);
 }
 
 function About() {
-  return <h2>About</h2>;
+  return (
+    <div>
+      <h2>我们的产品</h2>
+      <HeadCarouse></HeadCarouse>
+      <ImageList></ImageList>
+    </div>
+   
+);
 }
 
 function Products() {
   return (
         <div>
-          <h2>我们的产品</h2>
+          {/* <h2>我们的产品</h2> */}
           <HeadCarouse></HeadCarouse>
           <ImageList></ImageList>
+          <br></br>
+          <div>
+            联系电话：023-85287555；<br></br>
+            地址:重庆市璧山区永嘉大道102号（永嘉大桥桥头）
+          </div>
         </div>
+        
        
     );
 }
@@ -27,23 +48,27 @@ function App() {
   return (
     <Router>
       <div>
+
+      <Row>
+      <Col xs={{ offset:4}} sm={{ offset: 3 }} md={{ offset:3 }} lg={{offset: 10 }} xl={{ span: 4, offset: 10 }}><h1>重庆三丰旗舰店</h1></Col>
+     </Row>
         <nav>
           <ul>
-            <li>
-              <Link to="/">Home</Link>
+            {/* <li>
+              <Link to="/">我们的产品</Link>
             </li>
             <li>
-              <Link to="/about/">About</Link>
-            </li>
+              <Link to="/about/">我们的产品</Link>
+            </li> */}
             <li>
-              <Link to="/Products/">Products</Link>
+              <Link to="/Products/"></Link>
             </li>
           </ul>
         </nav>
-        <Route path="/" exact component={Index} />
-        <Route path="/about/" component={About} />
+        {/* <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} /> */}
         <Route path="/Products/" component={Products} />
-        
+        <Redirect to={"/Products/"}/>
       </div>
     </Router>
   );
